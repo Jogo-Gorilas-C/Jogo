e8009970edf09fdb328c359f87e5964510a05945
Lucas César <112576328+Lucc4s228@users.noreply.github.com>
2023-02-03 19:59:55 -0300

    Create console.h

---
+++ console.h
@@ -0,0 +1,9 @@
+#ifndef LAYOUT_H
+#define LAYOUT_H
+
+void turnos(esquerda);
+
+void layout(int coordenada, int distancia, int esquerda, char player1[10], char player2[10], int pontuacao1, int pontuacao2, int posicao_macaco1, int posicao_macaco2, int score1, int score2);
+
+
+#endif
