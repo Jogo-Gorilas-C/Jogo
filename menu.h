db9f67fe0f2ff371a63f23a69f06f373bb034c29
Lucas César <112576328+Lucc4s228@users.noreply.github.com>
2023-02-24 22:27:32 -0300

    Create menu.h

---
+++ menu.h
@@ -0,0 +1,7 @@
+#ifndef MENU_H_INCLUDED
+#define MENU_H_INCLUDED
+
+void menu(char* player1, char* player2, int pontuacao, int pontuacao1, int pontuacao2, int score1, int score2, int posicao_macaco1, int posicao_macaco2, char Rank[3][10], int pontos[3], int pontuacao_final1, int pontuacao_final2);
+
+
+#endif // MENU_H_INCLUDED
