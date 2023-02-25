a0bdb1f6d3888b81ea5026e9395997c992e2044b
Lucas César <112576328+Lucc4s228@users.noreply.github.com>
2023-02-24 22:33:33 -0300

    Create ranking.h

---
+++ ranking.h
@@ -0,0 +1,8 @@
+#ifndef RANKING_H_INCLUDED
+#define RANKING_H_INCLUDED
+
+void ranking(char Rank[][10], int pontos[]);
+
+void ordenador_rank(char Rank[][10], int pontos[], int pontuacao_final1, int pontuacao_final2, char player1[], char player2[]);
+
+#endif // RANKING_H_INCLUDED
