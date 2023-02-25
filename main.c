a9d4a62fa6d56fcab03c80b21b59cb9e5fad0b03
Lucas César <112576328+Lucc4s228@users.noreply.github.com>
2023-02-24 22:38:35 -0300

    Update main.c
    o menu tá na frente pq tentei resolver o problema do 'undefined reference to...', aparentemente deu certo, resta fazer com o resto

--- main.c
+++ main.c
@@ -3,14 +3,13 @@
 #include <string.h>
 #include <unistd.h>
 #include "console.h"
+#include "dificuldade.h"
+#include "jogo.h"
+#include "layout.h"
+#include "menu.h"
+#include "ranking.h"
+#include "jogo.h"
 
-int main(int argc, char *argv[]){
-
-//Variáveis
-#define LIMITE 156
-#define LIMITE_VERT 40
-#define PONTOS_ACERTO 100
-#define PONTOS_ERRO 1
 
 float POSICAO;
 int continuar;
@@ -19,12 +18,8 @@ int x = 2;
 int score1 = 2;
 int score2 = 2;
 int esquerda = 1;
-char macaco[1];
-strcpy(macaco, "O");
 char player1[10];
-strcpy(player1, "player1\0");
 char player2[10];
-strcpy(player2, "player2\0");
 int pontuacao1 = 0;
 int pontuacao2 = 0;
 int pontuacao = 0;
@@ -36,69 +31,7 @@ char Rank[3][10] = {"player1\0","player2\0","player3\0"};
 int pontos[3] = {0, 0, 0};
 
 
-
-//TUTORIAL
-
-void tutorial(char *player1, char *player2, int pontuacao, int pontuacao1, int pontuacao2, int score1, int score2, int posicao_macaco1, int posicao_macaco2, char rank[3][10], int pontos[3], int pontuacao_final1, int pontuacao_final2) {
-    clear();
-    imprimir_em((LIMITE / 2) - 4, 2, "Tutorial");
-    imprimir_em((LIMITE / 2) - 44, 10, "Os jogadores devem informar os seguintes valores: ângulo(em graus) e velocidade(em m/s)");
-    imprimir_em(1, LIMITE_VERT-4, "0 - PULAR TUTORIAL \n1 - CONTINUAR\n");
-    scanf("%d", &continuar);
-    system("pause");
-    if(continuar == 1){
-        clear();
-        imprimir_em((LIMITE / 2) - 4, 2, "Tutorial");
-        imprimir_em((LIMITE / 2) - 33, 10, "Lançamento falho: -1 ponto        Lançamento certeiro: +100 pontos");
-        imprimir_em(1, LIMITE_VERT-4, "0 - PULAR TUTORIAL \n1 - CONTINUAR\n");
-        scanf("%d", &continuar);
-        if(continuar == 1){
-            clear();
-            imprimir_linha(1, "=");
-            imprimir_em((LIMITE / 2) - 4, 2, "Tutorial");
-            imprimir_em((LIMITE / 2) - 41, 10, "Cada jogador possui 2 vidas, vence o jogador que acabar com as vidas do adversário");
-            imprimir_linha(3, "=");
-            imprimir_em(1, LIMITE_VERT-4, "1 - CONTINUAR\n");
-            scanf("%d", &continuar);
-            if(continuar == 1){
-                clear();
-                            }
-            else{
-                clear();
-            }
-        }
-        else{
-            clear();
-        }
-    }
-    else{
-        clear();
-    }
-}
-
-
-//RANKING
-
-void ranking(char Rank[][10], int pontos[]) {
-    int voltar_menu = 1;
-    int i;
-    while (voltar_menu != 0) {
-        clear();
-        imprimir_em((LIMITE / 2) - 6, 2, "RANKING");
-        for (i = 0; i < 3; i++) {
-            printf("\033[%d;%dH", 15 + i, LIMITE / 2 - 8);
-            printf("%d - %s : %d\n", i + 1, Rank[i], pontos[i]);
-            fflush(stdout);
-        }
-        imprimir_em(1, LIMITE_VERT - 4, "0 - VOLTAR\n");
-        scanf("%d", &voltar_menu);
-    }
-}
-
-
-//MENU
-
-void menu(char* player1, char* player2, int pontuacao, int pontuacao1, int pontuacao2, int score1, int score2, int posicao_macaco1, int posicao_macaco2, char rank[3][10], int pontos[3], int pontuacao_final1, int pontuacao_final2){
+void menu(char* player1, char* player2, int pontuacao, int pontuacao1, int pontuacao2, int score1, int score2, int posicao_macaco1, int posicao_macaco2, char Rank[3][10], int pontos[3], int pontuacao_final1, int pontuacao_final2){
 int x = 0;
 while(opcao != 1){
     clear();
@@ -139,17 +72,56 @@ while(opcao != 1){
 }
 
 
-//DIFICULDADE
 
-int dificuldade(){
-clear();
-imprima_em((LIMITE / 2) - 15, 2, "ESCOLHA A DIFICULDADE DO JOGO:");
-imprima_em((LIMITE / 2) - 25, 5, "1 - FÁCIL : distância entre os macacos é mostrada");
-imprima_em((LIMITE / 2) - 25, 8, "2 - MÉDIO : as posições dos macacos é mostrada");
-imprima_em((LIMITE / 2) - 25, 11, "3 - DIFÍCIL : distância  e posição não são mostrados\n");
-scanf("%d", &x);
+
+int main(int argc, char *argv[]){
+
+
+//Variáveis
+
+
+void tutorial(char *player1, char *player2, int pontuacao, int pontuacao1, int pontuacao2, int score1, int score2, int posicao_macaco1, int posicao_macaco2, char rank[3][10], int pontos[3], int pontuacao_final1, int pontuacao_final2) {
+    clear();
+    imprimir_em((LIMITE / 2) - 4, 2, "Tutorial");
+    imprimir_em((LIMITE / 2) - 44, 10, "Os jogadores devem informar os seguintes valores: ângulo(em graus) e velocidade(em m/s)");
+    imprimir_em(1, LIMITE_VERT-4, "0 - PULAR TUTORIAL \n1 - CONTINUAR\n");
+    scanf("%d", &continuar);
+    if(continuar == 1){
+        clear();
+        imprimir_em((LIMITE / 2) - 4, 2, "Tutorial");
+        imprimir_em((LIMITE / 2) - 33, 10, "Lançamento falho: -1 ponto        Lançamento certeiro: +100 pontos");
+        imprimir_em(1, LIMITE_VERT-4, "0 - PULAR TUTORIAL \n1 - CONTINUAR\n");
+        scanf("%d", &continuar);
+        if(continuar == 1){
+            clear();
+            imprimir_em((LIMITE / 2) - 4, 2, "Tutorial");
+            imprimir_em((LIMITE / 2) - 41, 10, "Cada jogador possui 2 vidas, vence o jogador que acabar com as vidas do adversário");
+            imprimir_em(1, LIMITE_VERT-4, "1 - CONTINUAR\n");
+            scanf("%d", &continuar);
+            if(continuar == 1){
+                clear();
+                menu(player1, player2, pontuacao, pontuacao1, pontuacao2, score1, score2, posicao_macaco1, posicao_macaco2, Rank[3][10], pontos[3], pontuacao_final1, pontuacao_final2);
+                }
+            else{
+                clear();
+                menu(player1, player2, pontuacao, pontuacao1, pontuacao2, score1, score2, posicao_macaco1, posicao_macaco2, Rank[3][10], pontos[3], pontuacao_final1, pontuacao_final2);
+                }
+        }
+        else{
+            clear();
+            menu(player1, player2, pontuacao, pontuacao1, pontuacao2, score1, score2, posicao_macaco1, posicao_macaco2, Rank[3][10], pontos[3], pontuacao_final1, pontuacao_final2);
+        }
+    }
+    else{
+        clear();
+        menu(player1, player2, pontuacao, pontuacao1, pontuacao2, score1, score2, posicao_macaco1, posicao_macaco2, Rank[3][10], pontos[3], pontuacao_final1, pontuacao_final2);    }
 }
+
+
+
+
+tutorial(player1, player2, pontuacao, pontuacao1, pontuacao2, score1, score2, posicao_macaco1, posicao_macaco2, Rank[3][10], pontos[3], pontuacao_final1, pontuacao_final2);
 
 return 0;
-}
-}
+}}
+
