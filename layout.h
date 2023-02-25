6372e88225b520d8d83366ba3172c90899878d32
Lucas César <112576328+Lucc4s228@users.noreply.github.com>
2023-02-24 22:35:16 -0300

    Create layout.h

---
+++ layout.h
@@ -0,0 +1,8 @@
+#ifndef LAYOUT_H_INCLUDED
+#define LAYOUT_H_INCLUDED
+
+void turnos(esquerda);
+
+void layout(int coordenada, int distancia, int esquerda, char player1[10], char player2[10], int pontuacao1, int pontuacao2, int posicao_macaco1, int posicao_macaco2, int score1, int score2);
+
+#endif // LAYOUT_H_INCLUDED
