509278ff7e6079077b786dab7d54116340ecd56c
Lucas César <112576328+Lucc4s228@users.noreply.github.com>
2023-02-24 22:31:57 -0300

    Create jogo.h

---
+++ jogo.h
@@ -0,0 +1,7 @@
+#ifndef JOGO_H_INCLUDED
+#define JOGO_H_INCLUDED
+
+void jogo(float coordenada, float distancia, int esquerda, char player1[], char player2[], int pontuacao[], int pontuacao1, int pontuacao2, int score1, int score2, float posicao_macaco1, float posicao_macaco2);
+
+
+#endif // JOGO_H_INCLUDED
