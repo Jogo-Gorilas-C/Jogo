c426984d3b3aa4c1e9eac89b66ca484249f695e8
Lucas César <112576328+Lucc4s228@users.noreply.github.com>
2023-02-24 22:30:11 -0300

    Update console.h

--- console.h
+++ console.h
@@ -1,9 +1,15 @@
-#ifndef LAYOUT_H
-#define LAYOUT_H
+#ifndef CONSOLE_H
+#define CONSOLE_H
 
-void turnos(esquerda);
 
-void layout(int coordenada, int distancia, int esquerda, char player1[10], char player2[10], int pontuacao1, int pontuacao2, int posicao_macaco1, int posicao_macaco2, int score1, int score2);
+void clear(); // limpar a tela
+void imprimir_em(int x, int y, char *texto);
+
+//constantes globais
+#define LIMITE 156
+#define LIMITE_VERT 40
+#define PONTOS_ACERTO 100
+#define PONTOS_ERRO 1
 
 
 #endif
