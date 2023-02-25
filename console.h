3aa7597ed2011b0eba1ca95731fe8df9d2a07443
Lucas César <112576328+Lucc4s228@users.noreply.github.com>
2023-02-25 10:52:32 -0300

    Update console.h

--- console.h
+++ console.h
@@ -1,15 +1,16 @@
-#ifndef CONSOLE_H
-#define CONSOLE_H
-
+#ifndef CONSOLE_H_INCLUDED
+#define CONSOLE_H_INCLUDED
 
 void clear(); // limpar a tela
+
 void imprimir_em(int x, int y, char *texto);
 
-//constantes globais
+void reset(int x, int y, int lines, int columns);
+
+//Variáveis globais
 #define LIMITE 156
 #define LIMITE_VERT 40
 #define PONTOS_ACERTO 100
 #define PONTOS_ERRO 1
 
-
-#endif
+#endif // CONSOLE_H_INCLUDED
